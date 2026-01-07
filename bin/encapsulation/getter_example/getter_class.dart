3d5b76b9cafd2f54a829e3f2c721ecca81e3ed2f
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2026-01-07 21:33:48 +0600

    dart encapsulation getter

---
+++ bin/encapsulation/getter_example/getter_class.dart
@@ -0,0 +1,11 @@
+// getter class
+class GetterClass {
+  // private properties
+  String _name = '';
+  int? _age;
+
+  // getter
+String get name => _name;
+int? get age => _age;
+
+}
---
+++ bin/encapsulation/getter_example/getter_object.dart
@@ -0,0 +1,8 @@
+// getter obejct
+import 'getter_class.dart';
+void main() {
+  GetterClass getterClass =GetterClass();
+  // get private properties or read value
+  print(getterClass.name);
+  print(getterClass.age);
+}
