eb4bfe61e791de98f225b4aa84b7ed46ef09cef0
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2026-01-31 23:20:31 +0600

    dart new code

---
+++ bin/inheritance/super_constructor/super_keyword_example_1/default_constructor_example/animal_class.dart
@@ -0,0 +1,20 @@
+// create animal class
+class Animal{
+String? name;
+
+// create constructor(default)
+Animal () {
+  print('Animal name is Dog');
+}
+}
+
+// create inheritance(default)
+class Dog extends Animal {
+  String? color;
+  // create constructor
+
+Dog() {
+  print('Dog is Animal');
+}
+
+}
---
+++ bin/inheritance/super_constructor/super_keyword_example_1/default_constructor_example/object_class.dart
@@ -0,0 +1,7 @@
+// create object
+import 'animal_class.dart';
+void main() {
+  var dog1 = Dog();
+  print('Dog name is = ${dog1.name = 'Pushi'}');
+  print('Dog Color is = ${dog1.color = 'Red'}');
+}
