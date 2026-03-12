96bbe0506e877ec769d3895096ffa2e0d3425439
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2026-03-12 19:23:52 +0600

    dart polymorphism

--- bin/polymorphism/polymorphism_full_example.dart
+++ bin/polymorphism/polymorphism_full_example.dart
@@ -6,16 +6,27 @@ class Teacher {
   }
 }
 
+// parent class with => inheritance
 class Student extends Teacher {
 @override
   void subject() {
   print('Subject name is English');
 }
+// other method
+void roll(int roll){
+  print('Student roll is $roll');
 }
-class AllStudent implements Teacher{
+}
+// parent class and => Abstraction
+class AllStudent implements Teacher,Student{
   @override
   void subject() {
     print('All subject add for Teacher');
+  }
+
+  @override
+  void roll(int roll) {
+    print('Student roll is = $roll');
   }
 
 }
@@ -25,4 +36,5 @@ void main() {
   student.subject();
   AllStudent allStudent = AllStudent();
   allStudent.subject();
+  allStudent.roll(5);
 }
