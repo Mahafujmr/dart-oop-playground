1de9412d043ab0aa1d87801e094e954fed28ef92
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2026-03-28 23:45:27 +0600

    dart enum example

--- bin/enum/enum_example_one.dart
+++ bin/enum/enum_example_one.dart
@@ -2,14 +2,24 @@
 enum Weather {
   sunny,
   rainy,
-  cloudy,
+  cloudy;
+
+
 }
 
 void main() {
+  // creation object
  Weather weather = Weather.sunny;
  if(weather == Weather.cloudy) {
    print('The sky is = cloudy');
  } else {
    print('This Result is non valid');
+ }
+  // creation object
+   Weather weatherTime = Weather.rainy;
+ if(weather == Weather.cloudy){
+   print('Success Result ');
+ } else {
+   print('Not Success Result');
  }
 }
