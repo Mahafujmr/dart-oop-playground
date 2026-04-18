29bea704d5c3612c73ba5f477b35044d3684a389
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2026-04-18 23:59:50 +0600

    dart async await

--- bin/async_await/async_await_example.dart
+++ bin/async_await/async_await_example.dart
@@ -4,6 +4,9 @@ void main() {
 }
 
 // async await
-void car()  {
+void car()  async{
   print('Your name is Tuhin');
+ await Future.delayed(Duration(seconds: 4));
+  print('Submit your name :');
+  print('Your name is TT');
 }
